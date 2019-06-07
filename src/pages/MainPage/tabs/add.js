cd2d4ca9fb1732f1814b242c3d46662a0a7805e4
klapaucius4 <klapaucius.mk@gmail.com>
2019-06-07 14:31:45 +0200

    map v2

--- src/pages/MainPage/tabs/add.js
+++ src/pages/MainPage/tabs/add.js
@@ -1,28 +1,28 @@
-// import React from 'react';
+import React from 'react';
 
-// import { IonHeader, IonToolbar, IonTitle, IonContent, IonIcon } from '@ionic/react';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonIcon } from '@ionic/react';
 
-// const AddPlaceTab = () => {
-//   return (
-//     <>
-//       <IonHeader>
-//         <IonToolbar>
-//           <IonTitle>Add Place</IonTitle>
-//         </IonToolbar>
-//       </IonHeader>
-//       <IonContent fullscreen>
-//         <h1>Add Place</h1>
-//       </IonContent>
-//     </>
-//   );
-// };
+const AddPlaceTab = () => {
+  return (
+    <>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle><IonIcon name="add-circle" /> Dodaj miejsce</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <h1>Dodaj miejsce</h1>
+      </IonContent>
+    </>
+  );
+};
 
-// export default AddPlaceTab;
-
-
+export default AddPlaceTab;
 
 
 
+
+/*
 import React, { Component, IonHeader, IonToolbar, IonTitle, IonContent, IonIcon } from 'react';
 import { Map, GoogleApiWrapper } from 'google-maps-react';
 
@@ -69,3 +69,4 @@ export class MapContainer extends Component {
 export default GoogleApiWrapper({
   apiKey: 'AIzaSyAd9yyQB-tPzlnJcZkFGajADWEPb6_HGQw'
 })(MapContainer);
+*/
